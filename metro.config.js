82a27cc49539c9b66009d90155c89ef216446335
HASSAN-RAMZAN59 <hasaanramzan59@gmail.com>
2026-07-31 17:12:32 +0500

    fix: enable cleartext traffic, configure metro-config and android build setup

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -8,3 +16,5 @@ module.exports = {
     }),
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
